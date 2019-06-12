c1ebbd25a79689675b1a688eeb0c5cca0a041e68
Annamaljar8 <41546575+Annamaljar8@users.noreply.github.com>
2019-06-12 12:07:23 +0200

    creating stocks component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-      <h1>The Stocks -Stock Component</h1>
+  <div class="col-sm-6">
+      <div class="card border-success mb-3">
+        <div class="card-header">
+            NAME
+            <small>(Prise: PRISE)</small>
+        </div>
+        <div class="card-body text-success">
+        <form class="form-row" style="text-align: end;">
+            <div class="form-group col-md-8">
+                <input
+                    type="number"
+                    class="form-control"
+                    placeholder="Quantity">
+            </div>
+            <div class="form-group col-md-4 ">
+                <button
+                    class="btn btn-success"
+                    >Buy</button>
+            </div>
+        </form>
+        </div>
+    </div>
   </div>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-      <h1>The Stocks Component</h1>
+  <div class="row">
+      <app-stock v-for="stock in stocks"></app-stock>
   </div>
 </template>
+
+<script>
+import Stock from './Stock.vue';
+    export default {
+        data() {
+            return {
+                stocks: [
+                    { id: 1, name: 'BMW', prise: 110 },
+                    { id: 2, name: 'Google', prise: 200 },
+                    { id: 3, name: 'Apple', prise: 250 },
+                    { id: 4, name: 'Twitter', prise: 8 },
+                ]
+            }
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>
